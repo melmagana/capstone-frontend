68cffe40900a8f01d285a7534b48ea862ec736ff
Melissa Magaña <mmel.mgna@gmail.com>
2020-05-12 15:27:11 -0500

    user can successfully login and register

--- src/RegisterLoginForm/index.js
+++ src/RegisterLoginForm/index.js
@@ -11,7 +11,19 @@ export default class RegisterLoginForm extends Component {
 			country: '',
 			email: '',
 			password: '',
-			shelter: false
+			shelter: false,
+			action: 'Login'
+		}
+	}
+	switchForm = () => {
+		if(this.state.action === 'Login') {
+			this.setState({
+				action: 'Register'
+			})
+		} else {
+			this.setState({
+				action: 'Login'
+			})
 		}
 	}
 	handleChange = (event) => {
@@ -19,51 +31,65 @@ export default class RegisterLoginForm extends Component {
 			[event.target.name]: event.target.value
 		})
 	}
+	handleSubmit = (event) => {
+		event.preventDefault()
+		if(this.state.action === 'Register') {
+			this.props.register(this.state)
+		} else {
+			this.props.login(this.state)
+		}
+	}
 	render() {
 		return(
 			<div className='RegisterLoginForm'>
-				<h2>Sign Up</h2>
-				<form>
+				<h2>{this.state.action}</h2>
+				<form onSubmit={this.handleSubmit}>
+				{
+					this.state.action === 'Register'
+					&&
 					<div>
-						<label>Name</label>
-						<input
-							type='text'
-							name='name'
-							placeholder='enter name'
-							value={this.state.name}
-							onChange={this.handleChange}
-						/>
+						<div>
+							<label>Name</label>
+							<input
+								type='text'
+								name='name'
+								placeholder='enter name'
+								value={this.state.name}
+								onChange={this.handleChange}
+							/>
+						</div>
+						<div>
+							<label>City</label>
+							<input
+								type='text'
+								name='city'
+								placeholder='enter city'
+								value={this.state.city}
+								onChange={this.handleChange}
+							/>
+						</div>
+						<div>
+							<label>State</label>
+							<input
+								type='text'
+								name='state'
+								placeholder='enter state'
+								value={this.state.state}
+								onChange={this.handleChange}
+							/>
+						</div>
+						<div>
+							<label>Country</label>
+							<input
+								type='text'
+								name='country'
+								placeholder='enter country'
+								value={this.state.country}
+								onChange={this.handleChange}
+							/>
+						</div>
 					</div>
-					<div>
-						<label>City</label>
-						<input
-							type='text'
-							name='city'
-							placeholder='enter city'
-							value={this.state.city}
-							onChange={this.handleChange}
-						/>
-					</div>
-					<div>
-						<label>State</label>
-						<input
-							type='text'
-							name='state'
-							placeholder='enter state'
-							value={this.state.state}
-							onChange={this.handleChange}
-						/>
-					</div>
-					<div>
-						<label>Country</label>
-						<input
-							type='text'
-							name='country'
-							placeholder='enter country'
-							value={this.state.country}
-							onChange={this.handleChange}
-						/>
-					</div>
+				}
 					<div>
 						<label>Email</label>
 						<input
@@ -84,17 +110,36 @@ export default class RegisterLoginForm extends Component {
 							onChange={this.handleChange}
 						/>
 					</div>
-					<div>
-						<label>Shelter?</label>
-						<input
-							type='checkbox'
-							name='shelter'
-							value={this.state.shelter}
-							onChange={this.handleChange}
-						/>
-					</div>
-					<button>Submit</button>
+					{
+						this.state.action === 'Register'
+						&&
+						<div>
+							<div>
+								<label>Shelter?</label>
+								<input
+									type='checkbox'
+									name='shelter'
+									value={this.state.shelter}
+									onChange={this.handleChange}
+								/>
+							</div>
+						</div>
+					}
+					<button type='Submit'>
+						{this.state.action === 'Login' ? 'Log In' : 'Sign Up'}
+					</button>
 				</form>
+				{
+					this.state.action === 'Login'
+					?
+					<p>
+						Not a member? Sign Up <span onClick={this.switchForm}>Here</span>
+					</p>
+					:
+					<p>
+						Already a member? Log In <span onClick={this.switchForm}>Here</span>
+					</p>
+				}
 			</div>
 		)
 	}
